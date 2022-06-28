ad9c2e2f012c825961ac3367100a845f4d9f4430
CF-Trail <106690765+CF-Trail@users.noreply.github.com>
2022-06-28 17:36:41 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
 if game.PlaceId == 2597632885 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/CF-Trail/SnoHax/main/games/snoday"))()
+elseif game.PlaceId == 5307215810 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CF-Trail/SnoHax/main/games/randomizer"))()
 end
