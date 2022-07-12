914be66203c96208cc954a2bcd2f36b85b16fb65
CF-Trail <106690765+CF-Trail@users.noreply.github.com>
2022-07-12 20:34:30 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,4 +2,7 @@ if game.PlaceId == 2597632885 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/CF-Trail/SnoHax/main/games/snoday"))()
 elseif game.PlaceId == 5307215810 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/CF-Trail/SnoHax/main/games/randomizer"))()
+elseif game.PlaceId == 1401417393 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/CF-Trail/SnoHax/main/games/RagdollSystemTest"))()
+
 end
